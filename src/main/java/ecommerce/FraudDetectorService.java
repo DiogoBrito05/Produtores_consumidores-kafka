d1b01aa65e561f291d15deb8404ca4ce2d772225
DiogoBrito21 <diogo.brito@sitiorecantodoqueijo.com.br>
2025-07-07 16:13:37 -0300

    FEAT: Extraindo uma camada de consumidor

--- src/main/java/ecommerce/FraudDetectorService.java
+++ src/main/java/ecommerce/FraudDetectorService.java
@@ -1,6 +1,7 @@
 package ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
@@ -9,39 +10,38 @@ import java.util.Properties;
 import java.util.UUID;
 
 public class FraudDetectorService {
+
+
     public static void main(String[] args) {
-     var consumer = new KafkaConsumer<String, String>(properties());
-     consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-     while(true){
-         var records = consumer.poll(Duration.ofMillis(100));
-         if(!records.isEmpty()){
-             System.out.println("Encontrei"+ records.count() +" registros");
-             for (var record : records){
-                 System.out.println("------------------------------------------------");
-                 System.out.println("Processadno new order, checando por uma fraude");
-                 System.out.println(record.key());
-                 System.out.println(record.value());
-                 System.out.println(record.partition());
-                 System.out.println(record.offset());
-                 try{
-                     Thread.sleep(5000);
-                 }catch(InterruptedException e ){
-                     e.printStackTrace();
-                 }
-                 System.out.println("Ordem foi processada");
-             }
-         }
-     }
+        var frudService = new FraudDetectorService();
+        var service = new KafkaService(FraudDetectorService.class.getSimpleName(),"ECOMMERCE_NEW_SERVICE",
+                frudService::parse);
+        service.run();
     }
+
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------------");
+        System.out.println("Processadno new order, checando por uma fraude");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try{
+            Thread.sleep(5000);
+        }catch(InterruptedException e ){
+            e.printStackTrace();
+        }
+        System.out.println("Ordem foi processada");
+    }
+
+
+
 
     private static Properties properties(){
         var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.11.0.35:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
         //ID ÚNICO QUE ESTOU GERENDO NESSA LINHA
-        //properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, EmailService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
+        //properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
         return properties;
     }
 }
