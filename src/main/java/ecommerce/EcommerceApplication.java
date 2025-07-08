49dc77dfe5b61931cbf42487a62d51610afe645c
DiogoBrito21 <diogo.brito@sitiorecantodoqueijo.com.br>
2025-07-08 11:26:28 -0300

    FEAT: adicionando a serialização com Gson

--- src/main/java/ecommerce/EcommerceApplication.java
+++ src/main/java/ecommerce/EcommerceApplication.java
@@ -1,25 +1,30 @@
 package ecommerce;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
+
 
 @SpringBootApplication
 public class EcommerceApplication {
 
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
-		try(var dispatcher = new KafkaDispatcher()){
-			for (var i = 0; i<10; i++) {
+		try(var orderDispatcher = new KafkaDispatcher<Order>()) {
+			try (var emailDispatcher = new KafkaDispatcher<String>()) {
+				for (var i = 0; i < 10; i++) {
 
-				var key = UUID.randomUUID().toString();
-				var value = key + ",67523,1234";
-				dispatcher.send("ECOMMERCE_NEW_ORDER", value, value);
-				var email = "Thank you for order! We are processing your order!";
-				//var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-				dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+					var userId = UUID.randomUUID().toString();
+					var orderId = UUID.randomUUID().toString();
+					var amount = new BigDecimal(Math.random() * 5000 + 1);
+					var order = new Order(userId, orderId, amount);
+					orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+					var email = "Thank you for order! We are processing your order!";
+					//var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+					emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+				}
 			}
-		};
-
+		}
 
 	}
 
---
+++ src/main/java/ecommerce/Order.java
@@ -0,0 +1,16 @@
+package ecommerce;
+
+import java.math.BigDecimal;
+
+public class Order {
+
+    private final String userId, orderId;
+    private final BigDecimal amout;
+
+    public  Order(String userId, String orderId, BigDecimal amout){
+        this.userId = userId;
+        this.orderId = orderId;
+        this.amout = amout;
+    }
+
+}
